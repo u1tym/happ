b2e785fe0515ff70fe5f7fe56dd0ca7791438178
yuichi toyama <yuichi-toyama@ase.co.jp>
2025-07-23 17:44:55 +0900

    修正中

--- server/job/amount/amount.py
+++ server/job/amount/amount.py
@@ -9,12 +9,34 @@ warnings.filterwarnings('ignore')
 #import os
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../lib")))
 
+from datetime import datetime as dtm
+
 from ppsqldb import Db
 #from ppsqldb import TableType
 
 from plog import Log
 
 from typing import Optional
+from typing import Self
+from typing import TypedDict
+
+class CardInfo(TypedDict):
+    cname: str
+    due: int
+    pay_month: int
+    pay_day: int
+
+class PayInfo(TypedDict):
+    st: dtm
+    ed: dtm
+    amount: int
+
+class AmountRec(TypedDict):
+    aid: Optional[str]
+    yymmdd: dtm
+    useful: str
+    cname: str
+    amount: int
 
 class Amount:
     _db: Optional[Db] = None
@@ -22,7 +44,7 @@ class Amount:
     last_error: str
 
     # コンストラクタを書いて
-    def __init__(self, db: Optional[Db] = None, logger: Optional[Log] = None):
+    def __init__(self: Self, db: Optional[Db] = None, logger: Optional[Log] = None):
         if db is not None:
             self._db = db
         else:
@@ -34,3 +56,44 @@ class Amount:
             self._lg = Log(0, "amount", ".")  # Logのデフォルトコンストラクタを使う場合
 
         self.last_error = ""
+
+    def _connect(self: Self) -> bool:
+        if self._db is None:
+            return False
+
+        res = self._db.connect()
+        if res != True:
+            return False
+
+        return True
+
+    def get_card_list(self: Self) -> list[CardInfo]:
+        """ cardリストの取得 """
+        result: list[CardInfo] = []
+
+        if self._db is None:
+            return result
+
+        return result
+
+    def add_amount(self: Self, rec: AmountRec) -> bool:
+        """ amountの追加 """
+        return False
+
+    def update_amount(self: Self, rec: AmountRec) -> bool:
+        """ amountの更新 """
+        return False
+
+    def delete_amount(self: Self, aid: int) -> bool:
+        """ amountの削除 """
+        return False
+
+    def get_amount_list(self: Self, st: dtm, ed: dtm) -> list[AmountRec]:
+        """ amountの取得 """
+        result: list[AmountRec] = []
+        return result
+
+    def get_payment(self: Self, base: dtm) -> list[PayInfo]:
+        """ payment情報の取得 """
+        result: list[PayInfo] = []
+        return result
--- server/job/amount/db.sql
+++ server/job/amount/db.sql
@@ -26,7 +26,7 @@ create table pln_rec (
 );
 
 create table amt_rec (
-    aid    char(8)   primary key, -- システム上の主キー
+    aid    char(8)   primary key, -- 識別子
 
     yymmdd date      not null, -- 使用日
     dnum   int       not null, -- 日毎の連番
@@ -34,8 +34,132 @@ create table amt_rec (
     dord   int       not null, -- 日毎の表示順
 
     useful text      not null, -- 用途
+    cid    char(8)   not null, -- 支払い方法
     amount int       not null, -- 金額
+
+    paydate date     not null, -- 支払い集計日
 
     udate  TIMESTAMP default CURRENT_TIMESTAMP, -- 更新日時
     delflg boolean   not null default false     -- 削除フラグ
 );
+
+create table card_rec (
+    cid       char(8)   primary key, -- 識別子
+    cname     text      not null,    -- 名称
+    due       int       not null,    -- 締め日
+    pay_month int       not null,    -- 支払い月
+    pay_day   int       not null,    -- 支払い日
+    udate     TIMESTAMP default CURRENT_TIMESTAMP
+);
+insert into card_rec
+(cid, cname, due, pay_month, pay_day)
+values
+  ('C0000001', 'cache',   0, 0, 0)
+, ('C0000002', 'PayPay', 29, 1, 27)
+, ('C0000003', 'auPay',  15, 1, 10)
+;
+
+alter table amt_rec add constraint fk_cid
+foreign key (cid) references card_rec(cid);
+
+
+
+
+
+-- 集計用SQL
+
+
+-- 種別毎の支払い日算出
+with
+tmp_tgt as (
+    select
+        '2025-07-15'::date as tgt,
+        'C0000003' as cid
+),
+tmp_pay as (
+	-- 支払い種別毎の支払日（今日、支出した場合の支払日）
+	select
+	  C.cid,           -- 識別子
+	  C.cname,         -- 名称
+	  case when C.due = 0
+	       then D.tgt
+	       when extract(day from D.tgt)::int <= C.due
+	       then make_date( extract( year from D.tgt + (C.pay_month || ' month')::interval)::int,
+	                       extract(month from D.tgt + (C.pay_month || ' month')::interval)::int,
+	                       C.pay_day )
+	       else make_date( extract( year from D.tgt + ((C.pay_month + 1) || ' month')::interval)::int,
+	                       extract(month from D.tgt + ((C.pay_month + 1) || ' month')::interval)::int,
+	                       C.pay_day )
+	  end as pay_date  -- 支払日
+	from card_rec as C, tmp_tgt as D
+)
+
+select * from tmp_pay, tmp_tgt where tmp_pay.cid = tmp_tgt.cid
+
+
+
+-- 当日基準での集計対象期間算出
+with tmp_pay as (
+	-- 支払い種別毎の支払日（今日、支出した場合の支払日）
+	select
+	  C.cid,           -- 識別子
+	  C.cname,         -- 名称
+	  case when C.due = 0
+	       then make_date( extract(year from now())::int,
+	                       extract(month from now())::int,
+	                       extract(day from now())::int )
+	       when extract(day from now())::int <= C.due
+	       then make_date( extract( year from now() + (C.pay_month || ' month')::interval)::int,
+	                       extract(month from now() + (C.pay_month || ' month')::interval)::int,
+	                       C.pay_day )
+	       else make_date( extract( year from now() + ((C.pay_month + 1) || ' month')::interval)::int,
+	                       extract(month from now() + ((C.pay_month + 1) || ' month')::interval)::int,
+	                       C.pay_day )
+	  end as pay_date  -- 支払日
+	from card_rec as C
+),
+
+tmp_calc_limit as (
+    -- 一番未来の支払日基準で集計対象限界を取得
+	select
+	  case when extract(day from max(pay_date)) <= 22
+	       then make_date( extract( year from max(pay_date))::int,
+	                       extract(month from max(pay_date))::int,
+	                       22 )
+	       else make_date( extract( year from max(pay_date) + interval '1 month')::int,
+	                       extract(month from max(pay_date) + interval '1 month')::int,
+	                       22 )
+	  end as to_date
+	from tmp_pay
+),
+
+tmp_list as (
+    -- リスト作成用の連番
+	select -1 as n
+	union
+	select 0 as n
+	union
+	select 1 as n
+	union
+	select 2 as n
+	union
+	select 3 as n
+),
+tmp_between_list as (
+    -- 集計対象候補
+	select make_date( extract( year from now() + (N.n || ' month')::interval)::int,
+	                  extract(month from now() + (N.n || ' month')::interval)::int,
+	                  23 ) as st,
+	       make_date( extract( year from now() + ((N.n + 1) || ' month')::interval)::int,
+	                  extract(month from now() + ((N.n + 1) || ' month')::interval)::int,
+	                  22 ) as ed
+	from tmp_list as N
+)
+
+select
+  BTW.st,  -- 集計開始日
+  BTW.ed   -- 集計終了日
+from tmp_between_list as BTW, tmp_calc_limit as CLC
+where now() <= BTW.ed
+  and BTW.st <= CLC.to_date
+order by BTW.st
